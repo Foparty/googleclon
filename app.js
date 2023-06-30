cadd4102ae8c1f5c5b4732425b6265ea8f763a9d
Foparty <alfonsomartingarcia@gmail.com>
2023-06-30 16:32:40 +0200

    logos & sidemenu

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const body = document.body;
 const configBtn = document.querySelector('.config');
 const appsButton = document.querySelector('.gridbutton');
 const appsMenu = document.querySelector('.apps-menu');
+const rightNav = document.querySelector('.right-nav');
+const sideMenuBtn = document.querySelector('.hamburguer');
+const sideCloseMenuBtn = document.querySelector('.close-hamburguer');
+const sideMenu = document.querySelector('.sidemenu-container');
 
 appsButton.addEventListener('click', () => {
 	appsMenu.classList.toggle('shown');
 });
 
-configBtn.addEventListener('click', () => {
-	if (body.classList == 'dark') {
-		body.classList = 'light';
-	} else {
-		body.classList = 'dark';
-	}
+sideMenuBtn.addEventListener('click', () => {
+	sideMenu.classList.toggle('shown');
+});
+sideCloseMenuBtn.addEventListener('click', () => {
+	sideMenu.classList.toggle('shown');
+});
+
+sideMenu.addEventListener('click', () => {
+	sideMenu.classList.toggle('shown');
 });
 
 if (
@@ -23,3 +30,22 @@ if (
 } else {
 	body.classList = 'light';
 }
+
+configBtn.addEventListener('click', () => {
+	if (body.classList == 'dark') {
+		body.classList = 'light';
+	} else {
+		body.classList = 'dark';
+	}
+});
+
+const device = navigator.userAgent;
+
+if (device.includes('iPhone' || 'iPad')) {
+	rightNav.style.display = 'none';
+	sideMenu.style.display = 'block';
+}
+// if (device.includes('iPad')) {
+// 	rightNav.style.display = 'none';
+// 	sideMenu.style.display = 'block';
+// }
